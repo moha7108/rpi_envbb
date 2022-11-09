63c0c6a564b2dd8b3725817c0c0c343874833b53
moha7108 <moha7108@protonmail.com>
2022-11-08 19:34:43 -0500

    main code body

---
+++ rpi_envbb.py
@@ -0,0 +1,16 @@
+import time
+from rpi_sensor_monitors import monitors
+
+
+
+if __name__ == '__main__':
+
+    env_sensor = monitors.BME608()
+
+    env_sensor.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except:
+        env_sensor.stop()
