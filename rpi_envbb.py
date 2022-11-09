851ac93b6c621e6a637c29b329aeddee540ebcf0
moha7108 <moha7108@protonmail.com>
2022-11-08 20:42:05 -0500

    push to csv function

--- rpi_envbb.py
+++ rpi_envbb.py
@@ -1,6 +1,20 @@
 import time
 from rpi_sensor_monitors import monitors
 
+def push_to_csv(csv_file, data):
+	""" """
+	fieldnames = [label for label, paremeter in data.items()]
+
+	if not os.path.isfile(csv_file):
+		with open(csv_file, 'w', newline='') as file:
+			writer = csv.DictWriter(file, fieldnames =fieldnames)
+			writer.writeheader()
+			writer.writerow(data)
+	else:
+		with open(csv_file, 'a', newline='') as file:
+			writer = csv.DictWriter(file, fieldnames =fieldnames)
+			writer.writerow(data)
+	print(f'data pushed to {csv_file}')
 
 
 if __name__ == '__main__':
@@ -8,15 +22,14 @@ if __name__ == '__main__':
     env_sensor = monitors.BME680()
     log_dir = env_sensor.log_file.rsplit('/',1)
     log_dir.pop()
-    print(log_dir)
-
-
+    csv_file = f'{log_dir}/envbb_data.csv'
 
 
     env_sensor.start()
 
     try:
         while True:
+            push_to_csv(csv_file, env_sensor.sensor_readings)
 
             time.sleep(1)
     except:
