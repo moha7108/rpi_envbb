d8a727e4d115a877bc40e6ce93671ef37419408b
moha7108 <moha7108@protonmail.com>
2022-11-08 20:10:36 -0500

    add method to get log directory

--- rpi_envbb.py
+++ rpi_envbb.py
@@ -6,11 +6,18 @@ from rpi_sensor_monitors import monitors
 if __name__ == '__main__':
 
     env_sensor = monitors.BME680()
+    log_dir = env_sensor.log_file.split('/',1)
+    log_dir.pop()
+    print(log_dir)
+
+
+
 
     env_sensor.start()
 
     try:
         while True:
+
             time.sleep(1)
     except:
         env_sensor.stop()
